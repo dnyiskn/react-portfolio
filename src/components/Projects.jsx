819708a753afb2f77ee312be20321cfaf4fe7f83
Daniel Isaksen <disaksen513@gmail.com>
2022-01-28 13:15:23 -0500

    Added Project component

---
+++ src/components/Projects.jsx
@@ -0,0 +1,23 @@
+import { projects } from "../data";
+
+function Projects() {
+  return (
+    <section>
+      <div>
+        {projects.map((project) => (
+          <a href={project.link} key={project.image}>
+            <div>
+              <img alt="gallery" src={project.image} />
+              <div>
+                <h2>{project.subtitle}</h2>
+                <h1>{project.title}</h1>
+                <p>{project.description}</p>
+              </div>
+            </div>
+          </a>
+        ))}
+      </div>
+    </section>
+  );
+}
+export default Projects;
