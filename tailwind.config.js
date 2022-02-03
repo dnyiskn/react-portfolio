9a85b638f969a9a3f7aed890d6892c53a29214a4
Daniel Isaksen <disaksen513@gmail.com>
2022-02-03 09:15:36 -0500

    Changed animation speeds for Slow Medium and Fast

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,9 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        "spin-slow": "spin 10.1s linear infinite",
-        "spin-med": "spin 10.7s linear infinite",
-        "spin-fast": "spin 10.5s linear infinite",
+        "spin-slow": "spin 40.1s linear infinite",
+        "spin-med": "spin 20.7s linear infinite",
+        "spin-fast": "spin 20.5s linear infinite",
       },
     },
   },
