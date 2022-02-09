115a8dc6dc362d0b1476f0b15ed0150323a06cb9
Daniel Isaksen <disaksen513@gmail.com>
2022-02-09 15:31:12 -0500

    Used Flex to change the projects to side by side on large screens

--- src/components/CircleAnimation.jsx
+++ src/components/CircleAnimation.jsx
@@ -1,10 +1,10 @@
 export default function CircleAnimation() {
   return (
     <>
-      <div className=" absolute mx-auto testing-circle z-1  bg-gradient-to-r from-blue-600 via-sky-300 to-teal-300 animate-spin-slow"></div>
-      <div className=" absolute mx-auto testing-circle z-2  bg-gradient-to-r via-transparent from-green-400 to-transparent animate-spin-med"></div>
-      <div className=" absolute mx-auto testing-circle z-3  bg-gradient-to-r via-transparent from-transparent to-red-400 animate-spin-fast"></div>
-      <div className=" absolute mx-auto testing-circle-block z-4"></div>
+      <div className=" absolute mx-auto animation-circle z-1  bg-gradient-to-r from-blue-600 via-sky-300 to-teal-300 animate-spin-slow"></div>
+      <div className=" absolute mx-auto animation-circle z-2  bg-gradient-to-r via-transparent from-green-400 to-transparent animate-spin-med"></div>
+      <div className=" absolute mx-auto animation-circle z-3  bg-gradient-to-r via-transparent from-transparent to-red-400 animate-spin-fast"></div>
+      <div className=" absolute mx-auto animation-circle-block z-4"></div>
     </>
   );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,13 +2,13 @@ import { projects } from "../data";
 
 function Projects() {
   return (
-    <section className="container flex flex-col max-w-screen-lg p-5 mx-auto">
+    <section className="container  max-w-screen-lg p-5 mx-auto">
       <h2 className="text-4xl mb-5">Projects</h2>
-      <div className="">
+      <div className="lg:flex lg:gap-x-4">
         {projects.map((project) => (
           <a href={project.link} key={project.image}>
-            <div>
-              <img className=" max-w-5/6" alt="gallery" src={project.image} />
+            <div className="max-w-5/6">
+              <img className="max-w-full" alt="gallery" src={project.image} />
               <div className="my-5 max-w-5/6 border-solid border-slate-50 rounded">
                 <h4 className="text-slate-400">{project.subtitle}</h4>
                 <h3 className="text-xl font-bold">{project.title}</h3>
